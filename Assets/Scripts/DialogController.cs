44620252c15308a4288acb6cf6e4550e65821c51
JBengey <jack.bengeyfield@gmail.com>
2020-12-02 19:03:01 +0000

    Finished all yarn dialogue, Added dialog canvas rotation to player, Added hiding of canvas upon exit.

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 using Yarn.Unity;
 
 
-public class DialogController_Test : MonoBehaviour
+public class DialogController : MonoBehaviour
 {
     public string characterName = "";
     public string talkToNode = "";
     public YarnProgram scriptToLoad;
     public DialogueRunner dialogueRunner; //refernce to the dialogue control
     public GameObject dialogueCanavas; //refernce to the canvas
-    Vector3 PostionSpeachBubble = new Vector3(0f, 0.0f, -0.6f);
-    public GameObject player;
+    Vector3 PostionSpeachBubble = new Vector3(0f, 0.0f, -0.4f);
+    public GameObject player,emptyDialogueHolder; //reference to the player and to an empty game object
 
 
     /// </summary>
     // Start is called before the first frame update
     void Start()
     {
-        if (scriptToLoad == null) Debug.LogError("NPC3D not set up with yarn scriptToLoad ", this);
-        if (string.IsNullOrEmpty(characterName)) Debug.LogWarning("NPC3D not set up with characterName", this);
-        if (string.IsNullOrEmpty(talkToNode)) Debug.LogError("NPC3D not set up with talkToNode", this);
+        if (scriptToLoad == null) Debug.LogError("Diaogue Controller not set up with yarn scriptToLoad ", this);
+        if (string.IsNullOrEmpty(characterName)) Debug.LogWarning("Diaogue Controller not set up with characterName", this);
+        if (string.IsNullOrEmpty(talkToNode)) Debug.LogError("Diaogue Controller not set up with talkToNode", this);
 
         if (dialogueRunner == null) Debug.LogError("dialogueRunner not set up", this);
         if (dialogueCanavas == null) Debug.LogError("Dialogue Canvas not set up", this);
@@ -43,6 +43,7 @@ public class DialogController_Test : MonoBehaviour
                 if (dialogueCanavas != null)
                 {
                     //move the Canvas to the object and off set
+                    dialogueCanavas.SetActive(true);
                     dialogueCanavas.transform.SetParent(transform.parent.transform); // use the root to prevent scaling
                     dialogueCanavas.GetComponent<RectTransform>().anchoredPosition3D = transform.parent.TransformVector(PostionSpeachBubble);
                 }
@@ -59,23 +60,30 @@ public class DialogController_Test : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("collider exit");
-        if (dialogueRunner.IsDialogueRunning)
+        if (other.gameObject.CompareTag("Player"))
         {
-            dialogueRunner.Stop();
+            //move the Canvas to a empty holding gameobject, out of view on exit. Conversations therefore dont remain when walking away.
+            dialogueCanavas.SetActive(false);
+            dialogueCanavas.transform.SetParent(emptyDialogueHolder.transform); // use the root to prevent scaling
+            dialogueCanavas.GetComponent<RectTransform>().anchoredPosition3D = transform.parent.TransformVector(PostionSpeachBubble);
+
+            if (dialogueRunner.IsDialogueRunning)
+            {
+                dialogueRunner.Stop();
+            }
         }
     }
 
 
 
-    //Update function to rotate canvas to always face the player?, and enable weight on animation to hae the character to speak facing the player
+    //Update function to rotate canvas to always face the player?, and enable weight on animation to have the character to speak facing the player
 
     private void Update()
     {
-        if (dialogueRunner.IsDialogueRunning)
+        if (dialogueRunner.IsDialogueRunning & dialogueCanavas.activeInHierarchy)
         {
             //Rotate the canvas towards the player
-
+            dialogueCanavas.transform.rotation = player.transform.rotation;
         }
     }
 
