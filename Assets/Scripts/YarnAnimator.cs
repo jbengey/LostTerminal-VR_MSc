bf4674a2f37907de707fea28402e70288754547a
JBengey <jack.bengeyfield@gmail.com>
2020-12-04 11:46:19 +0000

    Added script for common Yarn control Added 2 dialogue animations for Sally

---
+++ Assets/Scripts/YarnAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using System;
+
+public class YarnAnimator : MonoBehaviour
+{
+    public Animator animationController;
+
+    //Command to allow yarn to trigger a certain animation state
+    [YarnCommand("playAnimatorState")]
+    public void playAnimatorState(string animatorState)
+    {
+        if (animatorState != null)
+        {
+            animationController.CrossFade(animatorState,0.5f);
+        }
+    }
+   
+}
